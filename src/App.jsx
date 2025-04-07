05bd22b3f8c63e7bee9f8ced31dfc279bdccbc36
Raph <raph4sure007@gmail.com>
2025-04-07 23:31:35 +0100

    Implement ProtectedRoute component for authentication; update App component to use ProtectedRoute for app layout access; modify city data in JSON and adjust background image path in CSS

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./hooks/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 // import Logo from './pages/Logo';
 
 function App() {
@@ -24,7 +25,14 @@ function App() {
                             <Route path="pricing" element={<Pricing />} />
                             <Route path="product" element={<Product />} />
                             <Route path="login" element={<Login />} />
-                            <Route path="app" element={<AppLayout />}>
+                            <Route
+                                path="app"
+                                element={
+                                    <ProtectedRoute>
+                                        <AppLayout />
+                                    </ProtectedRoute>
+                                }
+                            >
                                 <Route
                                     index
                                     element={<Navigate replace to="cities" />}
